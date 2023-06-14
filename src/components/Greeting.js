9ea62529fa723bceae2cb8a7bc5cd9c038f02aef
bilals <bks1733@gmail.com>
2023-06-14 12:00:41 +0530

    Writing Our First Test.

---
+++ src/components/Greeting.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react/cjs/react.production.min";
+
+const Greeting = () => {
+  return (
+    <Fragment>
+      <div>
+        <h2>Hello World!</h2>
+
+        <p>Its good to see you!</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Greeting;
