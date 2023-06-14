108430e4effb030cb6de773ba42f5eb4a120600a
bilals <bks1733@gmail.com>
2023-06-14 16:19:07 +0530

    Testing User Interaction & State.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changedTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <Fragment>
       <div>
         <h2>Hello World!</h2>
 
-        <p>Its good to see you!</p>
+        {!changedText && <p>Its good to see you!</p>}
+        {changedText && <p>Changed!</p>}
+        <button onClick={changedTextHandler}>Change Text!</button>
       </div>
     </Fragment>
   );
